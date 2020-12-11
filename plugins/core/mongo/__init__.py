1b6c01138f831e492a0f265dc2e1074e0e06dca8
Lparksi <60086490+Lparksi@users.noreply.github.com>
2020-12-11 13:02:45 +0000

    fix MongoDB warn. rm user Module.

--- plugins/core/mongo/__init__.py
+++ plugins/core/mongo/__init__.py
@@ -8,30 +8,44 @@ MongoDB = False
 if MONGODB_URL is not None:
     if MONGODB_SHOW_URL:
         logger.debug(f"[MongoDB] 正在尝试连接：{MONGODB_URL}")
-    client = MongoClient(MONGODB_URL)
-    db = client['bot']
-    clo_groups = db['groups']
-    clo_users = db['users']
-    clo_teach = db['teach']
+        client = MongoClient(MONGODB_URL)
+        db = client['bot']
+        clo_groups = db['groups']
+        clo_users = db['users']
+        clo_teach = db['teach']
 
 
-    def get_db_bot():
-        return db
+        def get_db_bot():
+            return db
 
 
-    def get_clo_groups():
-        return clo_groups
+        def get_clo_groups():
+            return clo_groups
 
 
-    def get_clo_users():
-        return clo_users
+        def get_clo_users():
+            return clo_users
 
 
-    def get_clo_teach():
-        return clo_teach
+        def get_clo_teach():
+            return clo_teach
 
 
     MongoDB = True
 else:
     logger.warning("[MongoDB] 数据库URL 未配置")
     logger.warning("[MongoDB] 基于 MongoDB 的插件将不可用！")
+    def get_db_bot():
+        return 0
+
+
+    def get_clo_groups():
+        return 0
+
+
+    def get_clo_users():
+        return 0
+
+
+    def get_clo_teach():
+        return 0
