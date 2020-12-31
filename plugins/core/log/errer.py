91865dfcc3bbec3a8f8ffd7d51d1ca0f204da861
Lparksi <l.moluan@outlook.com>
2020-12-31 18:33:06 +0800

    clean.

--- plugins/core/log/errer.py
+++
@@ -1,2 +0,0 @@
-class LogLevelError(Exception):
-    pass
--- plugins/others/help.py
+++
@@ -1,8 +0,0 @@
-from nonebot import on_command, CommandSession
-
-
-@on_command('help', only_to_me=False)
-async def help_img(session: CommandSession):
-    # 2020/11/21
-    # https://s3.ax1x.com/2020/11/21/D1aPpR.jpg
-    await session.send("[CQ:image,file=https://s3.ax1x.com/2020/11/21/D1aPpR.jpg]")
